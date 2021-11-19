6b9f859c67c623dadb5a419d397bfdd654683008
matt <mtthw>
2021-11-18 20:46:31 -0500

    Fix wifi connecting to hidden, NTP update fixes

--- src/data_uploader.cpp
+++ src/data_uploader.cpp
@@ -12,6 +12,7 @@ extern SdFs sd;
 #include "TeensyTimerTool.h"
 extern TeensyTimerTool::PeriodicTimer can_log_timer;
 extern SD_CAN_Logger sd_logger;
+extern Config_Manager config;
 
 DataUploader::DataUploader(Client& in_internet_client, char* in_server, int in_port, int _max_log_to_upload):
     http_client(in_internet_client, in_server, in_port){ //, internet_client(&in_internet_client){
@@ -142,7 +143,7 @@ bool DataUploader::upload_file(char* file_name){
   http_client.endRequest();
   int status_code = http_client.responseStatusCode();
   #ifdef DEBUG
-    Serial.print("Sent request for unit info, got status code");
+    Serial.print("Sent request for unit info, got status code: ");
     Serial.println(status_code);
     delay(100);
   #endif
@@ -154,7 +155,7 @@ bool DataUploader::upload_file(char* file_name){
         delay(100);
       #endif
         if (internet_client->connect(server, port)) {
-          Serial.println("- connected to server");
+          Serial.println("connected to server");
           
           Serial.println("reading data file");
           sd_logger.no_write_file = true;
@@ -165,6 +166,8 @@ bool DataUploader::upload_file(char* file_name){
 
           char request_header[1400] = "POST /data_file/?log_name=";
           strcat(request_header, file_name);
+          strcat(request_header, "&unit_id=");
+          strcat(request_header, config.unit_number);
           strcat(request_header, " HTTP/1.1\nUser-Agent: Arduino/1.0\nContent-Type: text/plain\nContent-Length: ");
           strcat(request_header, file_size_str);
           strcat(request_header, "\n");
@@ -210,6 +213,7 @@ bool DataUploader::upload_file(char* file_name){
         sd_logger.reopen_file();
         can_log_timer.start();
         sd_logger.no_write_file = true;
+        internet_client->stop();
         return true;
       }
     }
